23672cf496e35e66434e08f931ca3cc85fa104cd
Lauren Anderson <laurands@amazon.com>
2022-08-28 02:07:26 -0400

    added additional allowed origin

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://192.168.254.136:3005"];
+const allowedOrigins = [
+  "http://192.168.254.136:3005",
+  "http://192.168.1.4:/3005",
+  "http://localhost:3005",
+  ];
 
 module.exports = allowedOrigins;
